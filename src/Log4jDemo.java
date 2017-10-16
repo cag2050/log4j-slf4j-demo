fab1d4d8fa24bfb16779920ff685b2b78a90a875
cag2050 <cag2050@163.com>
2017-10-16 16:48:14 +0800

    m,

--- src/Log4jDemo.java
+++ src/Log4jDemo.java
@@ -1,9 +1,17 @@
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class Log4jDemo {
     static Logger logger = Logger.getLogger(Log4jDemo.class.getName());
     public static void main(String[] args) {
         System.out.println("Log4jDemo");
-        logger.info("Log4jDemo log");
+//        程序设置日志级别
+//        logger.setLevel(Level.WARN);
+        logger.trace("Trace Message!");
+        logger.debug("Debug Message!");
+        logger.info("Info Message!");
+        logger.warn("Warn Message!");
+        logger.error("Error Message!");
+        logger.fatal("Fatal Message!");
     }
 }
